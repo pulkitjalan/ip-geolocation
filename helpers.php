5cfa033a9c64b5a89999190760d2a5120695a7f2
pulkit <pulkit1990@gmail.com>
2015-10-07 11:19:17 +0100

    added a helpers

---
+++ helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (! function_exists('geoip')) {
+    /**
+     * Get an instance of the current geoip.
+     *
+     * @return \PulkitJalan\GeoIP\GeoIP
+     */
+    function geoip($key = null)
+    {
+        if (is_null($key)) {
+            return app('geoip');
+        }
+
+        return $app('geoip')->get.ucwords(camel_case($key));
+    }
+}
