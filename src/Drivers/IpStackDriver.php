554ff983bde05ead2c1e1b5200edb03287cc28cd
Martijn Weghorst <m.weghorst@gmail.com>
2019-09-05 11:40:49 +0200

    Update IpStackDriver.php

--- src/Drivers/IpStackDriver.php
+++ src/Drivers/IpStackDriver.php
@@ -3,6 +3,7 @@
 namespace PulkitJalan\GeoIP\Drivers;
 
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Arr;
 use PulkitJalan\GeoIP\Exceptions\InvalidCredentialsException;
 
 class IpStackDriver extends AbstractGeoIPDriver
@@ -30,20 +31,20 @@ class IpStackDriver extends AbstractGeoIPDriver
     {
         $data = $this->getRaw($ip);
 
-        if (empty($data) || (array_get($data, 'latitude') === 0 && array_get($data, 'longitude') === 0)) {
+        if (empty($data) || (Arr::get($data, 'latitude') === 0 && Arr::get($data, 'longitude') === 0)) {
             return $this->getDefault();
         }
 
         return [
-            'city' => array_get($data, 'city'),
-            'country' => array_get($data, 'country_name'),
-            'countryCode' => array_get($data, 'country_code'),
-            'latitude' => (float) number_format(array_get($data, 'latitude'), 5),
-            'longitude' => (float) number_format(array_get($data, 'longitude'), 5),
-            'region' => array_get($data, 'region_name'),
-            'regionCode' => array_get($data, 'region_code'),
-            'timezone' => array_get($data, 'time_zone.id'),
-            'postalCode' => array_get($data, 'zip'),
+            'city' => Arr::get($data, 'city'),
+            'country' => Arr::get($data, 'country_name'),
+            'countryCode' => Arr::get($data, 'country_code'),
+            'latitude' => (float) number_format(Arr::get($data, 'latitude'), 5),
+            'longitude' => (float) number_format(Arr::get($data, 'longitude'), 5),
+            'region' => Arr::get($data, 'region_name'),
+            'regionCode' => Arr::get($data, 'region_code'),
+            'timezone' => Arr::get($data, 'time_zone.id'),
+            'postalCode' => Arr::get($data, 'zip'),
         ];
     }
 
@@ -74,6 +75,6 @@ class IpStackDriver extends AbstractGeoIPDriver
      */
     protected function getUrl($ip)
     {
-        return 'https://api.ipstack.com/'.$ip.'?access_key='.array_get($this->config, 'key');
+        return 'https://api.ipstack.com/'.$ip.'?access_key='.Arr::get($this->config, 'key');
     }
 }
