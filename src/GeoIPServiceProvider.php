5a111c03f2966c87b6cafc7cafa3f12c90732dfe
Pulkit Jalan <pulkitjalan@users.noreply.github.com>
2019-09-06 09:01:26 +0100

    Update GeoIPServiceProvider.php

--- src/GeoIPServiceProvider.php
+++ src/GeoIPServiceProvider.php
@@ -8,13 +8,6 @@ use PulkitJalan\GeoIP\Console\UpdateCommand;
 class GeoIPServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
      * Boot the service provider.
      */
     public function boot()
