f9f9fd038097adf8270bd1a003666eb6a86a0a5d
Pulkit Jalan <pulkit1990@gmail.com>
2020-10-07 15:47:29 +0100

    add test

--- tests/Drivers/TelizeDriverTest.php
+++ tests/Drivers/TelizeDriverTest.php
@@ -2,12 +2,36 @@
 
 namespace PulkitJalan\GeoIP\Tests\Drivers;
 
+use Mockery;
 use PulkitJalan\GeoIP\GeoIP;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Client as GuzzleClient;
 use PulkitJalan\GeoIP\Tests\AbstractTestCase;
 use PulkitJalan\GeoIP\Exceptions\InvalidCredentialsException;
 
 class TelizeDriverTest extends AbstractTestCase
 {
+    public function test_telize()
+    {
+        $config = [
+            'driver' => 'telize',
+            'telize' => [
+                'key' => 'test',
+            ],
+        ];
+
+        $client = Mockery::mock(GuzzleClient::class);
+
+        $client->shouldReceive('get')
+            ->once()
+            ->andReturn(new Response(200, [], json_encode(['city' => 'test'])));
+
+        $geoip = new GeoIP($config, $client);
+        $geoip = $geoip->setIp($this->validIp);
+
+        $this->assertEquals('test', $geoip->get('city'));
+    }
+
     public function test_telize_exception_without_key()
     {
         $config = [
